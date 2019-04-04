775b94ed4e0d4ef6ba634988cab06a53bcca4c37
Carly <carly.jenkinson65@googlemail.com>
2019-04-04 21:43:07 +0100

    solution for exercise 1

--- exercise_1/spec/fizzbuzz_spec.rb
+++ exercise_1/spec/fizzbuzz_spec.rb
@@ -9,15 +9,15 @@ describe FizzBuzz do
     it "returns a list of numbers" do
       expect(subject.play(2)).to eq "1\n2"
     end
-
+  
     it "returns Fizz when a number is divisible by 3" do
       expect(subject.play(3)).to eq "1\n2\nFizz"
     end
-
+  
     it "returns Buzz when a number is divisible by 5" do
       expect(subject.play(5)).to eq "1\n2\nFizz\n4\nBuzz"
     end
-
+  
     it "returns FizzBuzz when a number is divisible by 3 and 5" do
       expect(subject.play(15)).to eq "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz"
     end
